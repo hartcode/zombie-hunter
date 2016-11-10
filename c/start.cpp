2ce2ebf54a84879dd8ff85cdebc1813622c4f21a
Alex Hart <alex@hartcode.com>
2016-11-10 14:58:19 -0600

    Finish start screen
    We now have a title screen and a story screen

--- c/start.cpp
+++ c/start.cpp
@@ -6,14 +6,6 @@ using namespace std;
 
 void start_screen() {
   clear();
-  cout << "Booting";
-  for (int i = 0; i < 3; i++) {
-  sleep(100);
-  cout << '.';
-  }
-  cout << endl << "Online" << endl << endl << endl;
-  sleep(500);
-  clear();
 
   unsigned int rows = getRows();
   unsigned int cols = getCols();
@@ -22,10 +14,12 @@ void start_screen() {
   for (unsigned int x = 1; x < rows-1; x++) {
     cout <<' ';
     for (unsigned int y = 1; y < cols-1; y++) {
-      if (!first && x == rows / 2) {
-        if (!first && y > 10 && y < cols - 10) {
-          cout << "Zombie Hunter 100";
-          first = true;
+      if (x == rows / 2) {
+        if (y > cols /2 - ((21/2)+1) && y < cols /2 + ((21/2)+1)) {
+          if (!first) {
+            cout << "  Zombie Hunter 100  ";
+            first = true;
+          }
         } else {
           cout << (char)219;
         }
@@ -36,5 +30,19 @@ void start_screen() {
     cout << endl;
   }
   getkey();
+  clear();
 
+  cout << "The year is 2016 and the human race has fallen to zombies." << endl << endl;
+  sleep(1000);
+  cout << "In a desperate attempt to save humanity an advanced Artificial Intelligence was developed in hopes of finding a cure." << endl << endl;
+  sleep(2000);
+  cout << "A cure was found, but it was too late, the last humans have become mindless zombies." << endl << endl;
+  sleep(1000);
+  cout << "With the humans gone, the Artificial Intelligence created 'Zombie Hunter' class robots to deliver the cure to the humans." << endl << endl;
+  sleep(2000);
+  cout << "As a Zombie Hunter it's up to YOU to save humanity." << endl;
+  cout << endl << "Press Space to Continue" << endl;
+  while (getkey() != FIRE_KEY); {
+    sleep(100);
+  }
 }
