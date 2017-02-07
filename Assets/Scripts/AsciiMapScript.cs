463e88d0c98a9bee3a44f90dba5c42abe8ce548f
Alex Hart <alex@hartcode.com>
2017-02-07 16:16:51 -0600

    Add Ability to load more than one world tile

--- Assets/Scripts/AsciiMapScript.cs
+++ Assets/Scripts/AsciiMapScript.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
+using System.Text;
+using System;
 
 public class AsciiMapScript : MonoBehaviour
 {
@@ -14,15 +16,31 @@ public class AsciiMapScript : MonoBehaviour
 	public GameObject prefabWall;
 
 	protected MapData mapData;
+	protected MapFile mapfile;
+	protected int Worldx = 0;
+	protected int Worldy = 0;
+	protected int MapCols = 20;
+	protected int MapRows = 20;
+
+	String getMapPath(int x, int y) {
+		StringBuilder sb = new StringBuilder ("map");
+		sb.Append (x);
+		sb.Append ("x");
+		sb.Append (y);
+		sb.Append(".txt");
+		return sb.ToString ();
+	}
 
 	// Use this for initialization
 	void Start ()
 	{
 		mapData = new MapData ();
-		MapFile mapfile = new MapFile();
+		mapfile = new MapFile();
+	
+		String startingMapFile = getMapPath (Worldx, Worldy);
+		mapfile.SaveFile(mapData,startingMapFile);
 
-		mapfile.SaveFile(mapData,"map00.txt");
-		mapData = mapfile.LoadFile ("map00.txt");
+
 
 		if (prefabParent == null) {
 			prefabParent = GameObject.Find ("AsciiMapCharacters");
@@ -31,46 +49,58 @@ public class AsciiMapScript : MonoBehaviour
 		if (prefabWall == null) {
 			prefabWall = (GameObject)Resources.Load ("Main/Wall", typeof(GameObject));
 		}
-		for (int x = -1; x <= mapData.getCols(); x++) {
-			for (int y = -1; y <= mapData.getRows(); y++) {
 
-				if (x < 0 || x == mapData.getCols() || y < 0 || y == mapData.getRows()) {
-					CreateMapObject (x, y,prefabWall);
-				} else {
-					GameObject floorObject = mapData.getFloor (x, y);
-					if (floorObject != null) {
-						CreateMapObject (x, y,floorObject);
-					}
-					GameObject mainObject = mapData.getMain (x, y);
-					if (mainObject != null) {
-						CreateMapObject (x, y, mainObject);
-					}
-				}
-			}
-		}
+		LoadMap (Worldx, Worldy);
+		LoadMap (0, 1);
+		LoadMap (1, 0);
+		LoadMap (1, 1);
+
 		GameObject player = GameObject.Find ("Player");
 		if (player == null) {
 			throw new MissingReferenceException ("Player gameobject is missing");
 		}
 
 		// throw the player in the center of the map.
-		Vector3 playerPosition = calculateTransformPosition((int)(mapData.getCols() / 2),(int)(mapData.getRows() / 2));
+		Vector3 playerPosition = calculateTransformPosition((int)(mapData.getCols() / 2),(int)(mapData.getRows() / 2), Worldx, Worldy);
 		player.transform.position = playerPosition;
 	}
 
-	Vector3 calculateTransformPosition(int x, int y) {
+	Vector3 calculateTransformPosition(int x, int y, int Worldx, int Worldy) {
 		Vector3 retval;
-		retval = new Vector3 (OriginX + (x * CharacterWidth), OriginY + (-y * CharacterHeight), 0);
+		retval = new Vector3 (OriginX + (x * CharacterWidth) + (MapRows * CharacterWidth * Worldx), OriginY + (-y * CharacterHeight) + (MapCols * CharacterHeight * -Worldy), 0);
 		return retval;
 	}
 
-	void CreateMapObject (int x, int y, GameObject mapPrefab)
+	void CreateMapObject (int x, int y, GameObject mapPrefab, int Worldx, int Worldy)
 	{
 		if (mapPrefab != null) {
-			GameObject prefab = (GameObject)Instantiate (mapPrefab, calculateTransformPosition(x,y), Quaternion.identity, prefabParent.transform);
+			GameObject prefab = (GameObject)Instantiate (mapPrefab, calculateTransformPosition(x,y, Worldx, Worldy), Quaternion.identity, prefabParent.transform);
 			prefab.isStatic = true;
 		} else {
 			throw new MissingReferenceException ("Map Prefab Reference Missing");
+		}
+	}
+
+
+	void LoadMap(int Worldx, int Worldy) {
+		String mapPath = getMapPath (Worldx, Worldy); 
+		mapData = mapfile.LoadFile(mapPath);
+		for (int x = 0; x < mapData.getCols(); x++) {
+			for (int y = 0; y < mapData.getRows(); y++) {
+
+				if (x < 0 || x == mapData.getCols() || y < 0 || y == mapData.getRows()) {
+					CreateMapObject (x, y,prefabWall, Worldx, Worldy);
+				} else {
+					GameObject floorObject = mapData.getFloor (x, y);
+					if (floorObject != null) {
+						CreateMapObject (x, y,floorObject, Worldx, Worldy);
+					}
+					GameObject mainObject = mapData.getMain (x, y);
+					if (mainObject != null) {
+						CreateMapObject (x, y, mainObject, Worldx, Worldy);
+					}
+				}
+			}
 		}
 	}
 }
