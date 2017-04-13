b09b80bd8775cd2e5de2370101c78231a1de08b0
Alex Hart <alex@hartcode.com>
2017-04-13 12:43:26 -0500

    Escape to reset
    Also added zombies

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButton ("Reset")) {
+			SceneManager.LoadScene (1);
+		}
+	}
+}
