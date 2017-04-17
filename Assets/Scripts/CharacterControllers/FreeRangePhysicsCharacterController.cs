3e9bb259a3ba1b222cc0502c72e5a8a72c72aee6
Alex Hart <alex@hartcode.com>
2017-04-17 11:13:07 -0500

    Remove zombie code

--- Assets/Scripts/CharacterControllers/FreeRangePhysicsCharacterController.cs
+++ Assets/Scripts/CharacterControllers/FreeRangePhysicsCharacterController.cs
@@ -14,28 +14,6 @@ public class FreeRangePhysicsCharacterController : MyCharacterController
 		}
 	}
 
-	// Update is called once per frame
-	/*void FixedUpdate () {
-		if(Input.GetAxis("Vertical") > 0)//Press up arrow key to move up on the Y AXIS
-		{
-			direction = new Vector3 (0,1,0);
-
-		}
-		if(Input.GetAxis("Vertical") < 0)//Press up arrow key to move down on the Y AXIS
-		{
-			direction = new Vector3 (0,-1,0);
-		}
-		if(Input.GetAxis("Horizontal") < 0)//Press up arrow key to move left on the X AXIS
-		{
-			direction = new Vector3 (-1,0,0);
-		}
-		if(Input.GetAxis("Horizontal") > 0)//Press up arrow key to move right on the X AXIS
-		{   direction = new Vector3 (1,0,0);
-		}
-		Vector2 mydirection = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		myrigidbody2D.MovePosition(myrigidbody2D.position + mydirection * characterSpeed * Time.deltaTime);
-	}*/
-
 	// better directioning
 	void Update () {
 
